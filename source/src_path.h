f1a2c4191e5788f908f213ce3a25a27ba89d4a43
Mark Kim <mkim@sci.utah.edu>
2016-02-29 12:48:32 -0700

    Convert simple bump mapping from
    http://www.paulsprojects.net/tutorials/simplebump/simplebump.html
    
    to CMake.

---
+++ source/src_path.h
@@ -0,0 +1,13 @@
+
+#ifndef __SRC_PATH_H
+#define __SRC_PATH_H
+
+#include <string>
+#define shader_path "/home/mkim/Downloads/simplebump/source/Shaders/"
+//std::string shader_path = std::string("/home/mkim/Downloads/simplebump/Shaders/");
+
+#define texture_path "/home/mkim/Downloads/simplebump/source/textures/"
+//std::string texture_path = std::string("/home/mkim/Downloads/simplebump/textures/");
+
+#define model_path "/home/mkim/Downloads/simplebump/../data/"
+#endif
